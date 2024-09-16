2c5796985da60a36990f7eb32dbd38b7010e0d34
Pia <algauer.henrique@gmail.com>
2024-09-16 17:19:17 -0300

    primeiro teste unitário

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,20 +1,31 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { compare } from 'bcryptjs'
 import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
 
 describe('Register Use Case', () => {
     it('should hash user password upon registration', async () => {
-        const prismaUsersRepository = new PrismaUsersRepository()
-        const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+        const registerUseCase = new RegisterUseCase({
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            async findByEmail(_email) {
+                return null
+            },
+
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date()
+                }
+            }
+        })
 
         const { user } = await registerUseCase.execute({
             name: 'John Doe',
             email: 'Johndoe@email.com',
             password: '123456'
         })
-
-        console.log(user.password_hash)
 
         const isPasswordHashed = await compare(
             '123456',
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [tsconfigPaths({
-        loose: true,
-        root: './src'
-    })]
+    plugins: [tsconfigPaths()],
+    // plugins: [tsconfigPaths({
+    //     loose: true,
+    //     root: './src'
+    // })],
+    resolve: {
+        alias: [
+            { find: "@/", replacement: resolve(__dirname, "./src") }
+        ]
+    },
 });
