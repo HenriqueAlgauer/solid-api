64a7cf61988147fdca60a1d1e65741b02ab4f770
henriqueAlgauer <algauer.henrique@gmail.com>
2024-09-25 21:49:31 -0300

    criado middleware de autenticação

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -2,11 +2,8 @@ import "@fastify/jwt"
 
 declare module "@fastify/jwt" {
     interface FastifyJWT {
-        payload: { id: number }
         user: {
-            id: number,
-            name: string,
-            age: number
+            sub: string
         }
     }
 }
