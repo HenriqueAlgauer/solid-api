36bf5eb701d58272737e336f98231fcda77a94b8
Pia <algauer.henrique@gmail.com>
2024-11-17 23:54:38 -0300

    criado rota e controllers checkin

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeFetchCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-check-ins-history'
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+    const checkInHistoryQueySchema = z.object({
+        page: z.coerce.number().min(1).default(1),
+    })
+
+    const { page } = checkInHistoryQueySchema.parse(request.query)
+
+    const fetchCheckInsHistoryUseCase = makeFetchCheckInsHistoryUseCase()
+
+    const { checkIns } = await fetchCheckInsHistoryUseCase.execute({
+        userId: request.user.sub,
+        page
+    })
+
+    return reply.status(200).send(
+        checkIns,
+    )
+}
