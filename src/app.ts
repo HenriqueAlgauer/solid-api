0ea095d0bb1d34dba1cdbd628e2db2600762eba7
Henrique Machado Algauer <102268957+HenriqueAlgauer@users.noreply.github.com>
2024-11-19 13:03:05 +0000

    implementado refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { usersRoutes } from './http/controllers/users/routes'
@@ -10,8 +11,9 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
-
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
