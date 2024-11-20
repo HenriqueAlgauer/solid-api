5fbff671f8b0216b82add0b2ae9cd3135187fcb3
Pia <algauer.henrique@gmail.com>
2024-11-20 10:43:47 -0300

    alias de importação fixed

--- utils/test/create-and-authenticate-user.ts
+++ utils/test/create-and-authenticate-user.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma"
+import { prisma } from "../../src/lib/prisma"
 import { hash } from "bcryptjs"
 import { FastifyInstance } from "fastify"
 import request from 'supertest'
@@ -9,7 +9,7 @@ export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = 
             name: 'zezinho',
             email: 'zezinho@email.com',
             password_hash: await hash('123456', 6),
-            role: isAdmin ? 'ADMIN' : 'MEMBER' 
+            role: isAdmin ? 'ADMIN' : 'MEMBER'
         }
     })
 
